a8755753ec4ab30f071839adcd018899aa2d8706
Nicholai de Guzman <Nicholai@cchinfatt.local>
2015-06-14 16:18:08 -0500

    controller patch method testing

--- spec/controllers/contact_controller_spec.rb
+++ spec/controllers/contact_controller_spec.rb
@@ -50,37 +50,112 @@ describe ContactsController do
   end
   
   describe "GET #new" do
-    it "assigns the requested contact to @contact"
-    it "renders the :new template"
+    it "assigns the requested contact to @contact" do
+      get :new
+      expect(assigns(:contact)).to be_a_new(Contact)
+    end
+      
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
     
   end
 
   describe "GET #edit" do
-    it "assigns the requested contact to @contact"
-    it "renders the :edit template"
+    it "assigns the requested contact to @contact" do
+      contact = create(:contact)
+      get :edit, id: contact
+      expect(assigns(:contact)).to eq contact
+    end
+    
+    it "renders the :edit template" do
+      contact = create(:contact)
+      get :edit, id: contact
+      expect(response).to render_template :edit  
+    end
+    
   end
   
   describe "POST #create" do
+    before :each do
+      @phones = [
+        attributes_for(:phone),
+        attributes_for(:phone),
+        attributes_for(:phone)
+      ]
+      
+    end
+    
     context "with valid attributes" do
-      it "saves the new contact in the database"
-      it "redirects to contacts#show"
+      it "saves the new contact in the database" do 
+        expect{
+          post :create, contact: attributes_for(:contact, phones_attributes: @phones)
+        }.to change(Contact, :count).by(1)
+      end
+      
+       it "redirects to contacts#show" do
+         post :create, contact: attributes_for(:contact, phones_attributes: @phones)
+         expect(response).to redirect_to contact_path(assigns[:contact])
+       end
     end
     
     context "with invalid attributes" do
-      it "does not save the new contact in the database"
-      it "re-renders the :new template"
+      it "does not save the new contact in the database" do
+        expect{
+          post :create, contact: attributes_for(:invalid_contact)
+        }.not_to change(Contact, :count)
+      end
+      
+      it "re-renders the :new template" do
+        post :create, 
+          contact: attributes_for(:invalid_contact)
+        expect(response).to render_template :new    
+      end
     end
+    
   end
   
-  describe "PATH #update" do
+  describe "PATCH #update" do
+    
+    before :each do
+      @contact = create(:contact, firstname: 'Lawrence', lastname: 'Smith')
+    end
+    
     context "with valid attributes" do
-      it "updates the contact in the database"
-      it "redirects to the contact"
+      
+      it "locates the requested @contact" do
+        patch :update, id: @contact, contact: attributes_for(:contact)
+        expect(assigns(:contact)).to eq(@contact)
+      end
+      
+      it "changes @contact's attributes" do
+        patch :update, id: @contact,
+          contact: attributes_for(:contact, firstname: 'Larry', lastname: 'Smith')
+        @contact.reload
+        expect(@contact.firstname).to eq('Larry')
+        expect(@contact.lastname).to eq('Smith')
+      end
+      
+      it "redirects to the updated contact" do
+        patch :update, id: @contact, contact: attributes_for(:contact)
+        expect(response).to redirect_to @contact
+      end
     end
     
     context "with invalid attributes" do
-      it "does not update the contact"
-      it "re-renders the :edit template"
+      it "does not change the contact's attributes" do
+        patch :update, id: @contact,
+          contact: attributes_for(:contact, firstname: 'Larry', lastname: nil)
+          @contact.reload
+        expect(@contact.firstname).not_to eq('Larry')
+        expect(@contact.lastname).to eq('Smith')
+      end
+        
+      it "re-renders the :edit template" do
+        patch :update, id: @contact, contact: attributes_for(:invalid_contact)
+        expect(response).to render_template :edit   
+      end
     end
   end
 
