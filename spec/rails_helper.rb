6674d6fb95bd8cc0b5d8632320a0b9979b7d7945
Nicholai de Guzman <Nicholai@cchinfatt.home>
2015-06-20 00:10:24 -0500

    finished with controller cleanup, but had to modify rails_helper to include the Dir and LoginMacros

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,6 +24,8 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   #config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -48,5 +50,6 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
   config.include FactoryGirl::Syntax::Methods
+  config.include LoginMacros
   
 end
